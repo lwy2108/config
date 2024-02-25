65037fbcfca260a1b3c37c2eb344a2f1bd016711
lwy2108 <88315275+lwy2108@users.noreply.github.com>
2024-02-25 23:21:01 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,23 +2,25 @@
 
 # install with preseed
 # change hostname
-# contrib non-free non-free-firmware
-# sudo apt install xinit xscreensaver openbox chromium udisks2 
+# contrib non-free
+# sudo apt install xinit slock openbox chromium udisks2 xterm 
 # use dracula 
+# https://bbs.archlinux.org/viewtopic.php?id=244316
 # sudo apt install openssh-server -y
 # -https://stackoverflow.com/questions/20898384/disable-password-authentication-for-ssh
 # -https://github.com/fail2ban/fail2ban/issues/3292#issuecomment-1142503461
 # use win10 image
-# dark reader, ublock, dracula
+# dark reader, ublock
 # http://google.com/search?q=%s
 
-sudo apt install nvidia-driver firmware-misc-nonfree -y &&
+sudo apt update &&
+sudo apt install nvidia-driver -y &&
 sudo dpkg --add-architecture i386 &&
 sudo apt update &&
 sudo apt install nvidia-driver-libs:i386 -y &&
 sudo apt install nvidia-cuda-dev nvidia-cuda-toolkit -y &&
 # ? sudo apt install mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386 &&
-sudo apt install vim keepassxc python3-pip python3-venv portaudio19-dev git nodejs npm ffmpeg telegram-desktop qemu-system libvirt-daemon-system virt-manager feh transmission torbrowser-launcher flatpak steam-installer unzip solaar gtk-theme-switch scrot -y &&
+sudo apt install vim keepassxc python3-pip python3-venv portaudio19-dev git nodejs npm ffmpeg telegram-desktop qemu-system libvirt-daemon-system virt-manager feh transmission torbrowser-launcher flatpak steam-installer unzip logiops pavucontrol man -y &&
 flatpak remote-add flathub https://dl.flathub.org/repo/flathub.flatpakrepo &&
 flatpak install flathub md.obsidian.Obsidian -y &&
 flatpak install flathub com.vscodium.codium -y &&
@@ -26,4 +28,4 @@ mkdir ~/pycharm &&
 tar xzf ../pycharm*.tar.gz -C ~/pycharm &&
 sh ~/pycharm/pycharm-community-2023.3.3/bin/pycharm.sh &&
 sh ../tws-stable-linux-x64.sh 
-
+echo "--------- REBOOT REQUIRED ---------"
